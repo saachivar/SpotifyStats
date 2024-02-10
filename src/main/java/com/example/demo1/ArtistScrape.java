709811d53bd17b40358e6ab2bc96070d6e7499f5
Saachi Varshney <saachi.varshney@gmail.com>
2024-02-10 12:46:37 -0500

    web scraping yayyy

--- src/main/java/com/example/demo1/ArtistScrape.java
+++ src/main/java/com/example/demo1/ArtistScrape.java
@@ -27,25 +27,29 @@ public class ArtistScrape extends VBox {
     public void scrapeBillBoard() {
         try {
             // Connect to the website and get the HTML
-            Document document = Jsoup.connect("https://www.songkick.com/leaderboards/trending_artists").get();
+            Document document = Jsoup.connect("https://www.billboard.com/charts/artist-100/").get();
 
             // Extract elements you're interested in
-            Elements elements = document.select("table.event-listings tbody tr"); // Select all <h1> elements
+            Elements elements = document.select("h3"); // Select all <h1> elements
 
             Text[] artists = new Text[elements.size()];
+            System.out.println(elements.size());
 
             // Loop through the elements and create Text objects
-            for (int i = 0; i < elements.size(); i++) {
+            for (int i = 4; i < 25; i+=2) {
                 // Select the element with class "name" within the row
                 Element element = elements.get(i);
-                Element nameElement = element.selectFirst(".name");
-                if (nameElement != null) {
+              //  Element nameElement = element.selectFirst("h3");
+                if (element != null) {
                     String text = element.text();
+
                     System.out.println(text);
 
                     // Create a Text object and add it to the array
                     artists[i] = new Text(text);
                     this.getChildren().add(artists[i]);
+                } else {
+                    System.out.println("null");
                 }
 
             }
