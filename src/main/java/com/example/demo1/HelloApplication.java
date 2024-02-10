ecccb14349d8e425136b9e3d7206d98eed09a042
sr64205 <sr64205@uga.edu>
2024-02-09 23:02:00 -0500

    check

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -76,7 +76,7 @@ public class HelloApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("ad;fkfkkkhjhji");
+        System.out.println(" hiiiiii ");
         launch();
     }
 }
