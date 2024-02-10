21629a7a16325e538cf47f7113d6acec0666a0d0
Saachi Varshney <saachi.varshney@gmail.com>
2024-02-09 22:23:57 -0500

    asl;kdgjk

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -23,7 +23,7 @@ public class HelloApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("ad;fk");
+        System.out.println("ad;ffk");
         launch();
     }
 }
