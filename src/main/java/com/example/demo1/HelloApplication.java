d8858ef86973b5e564802a6fbd8b89f88f8ddc18
sr64205 <sr64205@uga.edu>
2024-02-09 21:57:43 -0500

    commit2

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -23,7 +23,8 @@ public class HelloApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("shruthi");
+        System.out.println("shruthii" +
+                "");
         launch();
     }
 
