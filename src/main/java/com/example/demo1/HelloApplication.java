0120a412d6b4f3441b1d3aedbf97a98b4c437aa5
sr64205 <sr64205@uga.edu>
2024-02-10 10:24:35 -0500

    getting songs1312

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -182,7 +182,7 @@ public class HelloApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("ad;fkfkkkhjhji");
+        System.out.println("ad;idfgdfg");
         launch();
     }
 }
