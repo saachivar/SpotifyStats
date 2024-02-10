664553ea3bba91cb0e4d270894091cbe9000964f
Saachi Varshney <saachi.varshney@gmail.com>
2024-02-09 22:31:12 -0500

    vbox and hbox

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.demo1;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,24 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+
+        // Creating an HBox as the root node
+        HBox root = new HBox();
+
+        // Creating two VBox nodes
+        VBox leftVBox = new VBox();
+        VBox rightVBox = new VBox();
+
+        // Adding some content to the VBox nodes (for demonstration purposes)
+        leftVBox.getChildren().add(new javafx.scene.control.Label("Left VBox"));
+        rightVBox.getChildren().add(new javafx.scene.control.Label("Right VBox"));
+
+        // Adding the VBox nodes as children to the HBox
+        root.getChildren().addAll(leftVBox, rightVBox);
+
+        Scene scene = new Scene(root, 320, 240);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
