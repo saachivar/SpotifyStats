9200aa0c550b76ee57490841acf3c231d1c72ac5
Keerthur11 <kr36621@uga.edu>
2024-02-09 21:50:37 -0500

    commitkeerthana

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -23,7 +23,7 @@ public class HelloApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Keerthana");
+        System.out.println("Keerthanaaa");
         launch();
     }
 }
